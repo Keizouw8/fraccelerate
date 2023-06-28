1bd1ea079fabf11e2ec1607e158eee8e0d00aefb
keizouw8 <keizouw8@gmail.com>
2023-06-27 20:00:00 -0700

    feat: sidebar rendering

---
+++ build.ps1
@@ -0,0 +1,3 @@
+g++ -c main.cpp -o build\bin.o -IC:\SFML\include
+g++ build\bin.o -o build\bin.exe -LC:\SFML\lib -lsfml-graphics -lsfml-window -lsfml-system -lopengl32 -lwinmm -lgdi32
+.\build\bin.exe
--- main.cpp
+++ main.cpp
@@ -1,7 +1,46 @@
-#include "SFML/Graphics.hpp"
+#include <SFML/Graphics.hpp>
 #include <iostream>
-
+ 
 int main(){
-    std::cout << "Hello World!";
-    return 1;
+    sf::RenderWindow window(sf::VideoMode(800, 600), "fraccelerate");
+    sf::View defaultView = window.getDefaultView();
+
+    sf::Image icon;
+    icon.loadFromFile("files/icon.jpg");
+    window.setIcon(icon.getSize().x, icon.getSize().y, icon.getPixelsPtr());
+    
+    sf::RectangleShape sidebar(sf::Vector2f(300, 600));
+    sidebar.setFillColor(sf::Color(30, 30, 30));
+    sidebar.setPosition(0, 0);
+
+    sf::Font lexend;
+    lexend.loadFromFile("files/lexend.ttf");
+
+    sf::Text title;
+    title.setFont(lexend);
+    title.setString("fraccelerate");
+    title.setCharacterSize(20);
+    title.setPosition(10, 10);
+    title.setFillColor(sf::Color::White);
+
+    while(window.isOpen()){
+        window.clear();
+        sf::Event event;
+
+        while(window.pollEvent(event)){
+            if(event.type == sf::Event::Closed) window.close();
+            if(event.type == sf::Event::Resized){
+                defaultView.setSize(sf::Vector2f((float)event.size.width, (float)event.size.height));
+                defaultView.setCenter(event.size.width/2, event.size.height/2);
+                sidebar.setSize(sf::Vector2f(300, event.size.height));
+                window.setView(defaultView);
+            }
+        }
+
+        window.draw(sidebar);
+        window.draw(title);
+        window.display();
+    }
+
+    return 0;
 }
